064a6a118d92b4bf3e2ccc17848b0b05adcf2fe3
Manthan3000 <manthanovhal695@gmail.com>
2024-06-16 20:18:28 +0530

    main

--- JAVA 10 - 2D ARRAYS/arrays2D.java
+++ JAVA 10 - 2D ARRAYS/arrays2D.java
@@ -31,6 +31,40 @@ import java.util.*;
 
 // // Q take a matrix as input from the user Search for a given number X and print index
 
+// public class arrays2D {
+
+//     @SuppressWarnings("resource")
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Numbers of Rows :");
+//         int rows = sc.nextInt();
+//         System.out.print("Enter Numbers of Columns :");
+//         int columns = sc.nextInt();
+
+//         int[][] Numbers = new int[rows][columns];
+
+//         // rows
+//         for (int i = 0; i < rows; i++) {
+//             // Columns
+//             for (int j = 0; j < columns; j++) {
+//                 Numbers[i][j] = sc.nextInt();
+
+//             }
+//         }
+//         System.out.print("Enter Value Of X = ");
+//         int x = sc.nextInt();
+
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < columns; j++) {
+
+//                 if (Numbers[i][j] == x) {
+//                     System.out.println(x + " Found at locaton " + "(" + i + " , " + j + ")");
+//                 }
+//             }
+//         }
+//     }
+// }
+
 public class arrays2D {
 
     @SuppressWarnings("resource")
@@ -51,15 +85,10 @@ public class arrays2D {
 
             }
         }
-        System.out.print("Enter Value Of X = ");
-        int x = sc.nextInt();
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-
-                if (Numbers[i][j] == x) {
-                    System.out.println(x + " Found at locaton " + "(" + i + " , " + j + ")");
-                }
+        for (int j = 0; j < columns; j++) {
+            for (int i = 0; i < rows; i++) {
+                System.out.print(Numbers[i][j] + " ");
             }
         }
     }
