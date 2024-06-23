fbc6f9bac5c170c442445b1d27463b88f5d4bbbe
Manthan3000 <manthanovhal695@gmail.com>
2024-06-23 18:16:41 +0530

    main

--- JAVA 17 - RECURSION/recursion.java
+++ JAVA 17 - RECURSION/recursion.java
@@ -56,49 +56,49 @@
 
 // Q 4 :    PRINT FACTORIAL OF N NUMBERS
 
-// public class recursion {
-
-//     public static int calcFact(int n) {
-
-//         if (n == 1 || n == 0) { 
-//             return 1;
-//         }
-
-//         int fact_nm1 = calcFact(n - 1);
-//         int fact_n = n * fact_nm1;
-//         return fact_n;
-//     }
-
-//     public static void main(String[] args) {
-//         int n = 5;
-//         int ans = calcFact(n);
-//         System.out.println(ans);
-//     }
-// }
-
-// Q 5      FIBONACCI SERIES
-
 public class recursion {
 
-    public static void printFib(int a, int b, int n) {
-        if (n == 0) {
-            return;
+    public static int calcFact(int n) {
+
+        if (n == 1 || n == 0) {
+            return 1;
         }
-        int c = a + b;
-        System.out.println(c);
-        printFib(b, c, n - 1);
+
+        int fact_nm1 = calcFact(n - 1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
     }
 
     public static void main(String[] args) {
-        int a = 0, b = 1;
-        System.out.println(a);
-        System.out.println(b);
-        int n = 7;
-
-        printFib(a, b, n - 2);
+        int n = 5;
+        int ans = calcFact(n);
+        System.out.println(ans);
     }
-
 }
+
+// Q 5 FIBONACCI SERIES
+
+// public class recursion {
+
+// public static void printFib(int a, int b, int n) {
+// if (n == 0) {
+// return;
+// }
+// int c = a + b;
+// System.out.println(c);
+// printFib(b, c, n - 1);
+// }
+
+// public static void main(String[] args) {
+// int a = 0, b = 1;
+// System.out.println(a);
+// System.out.println(b);
+// int n = 7;
+
+// printFib(a, b, n - 2);
+// }
+
+// }
 
 // Q 6 PRINT X ^ N (stack height = n)
 
