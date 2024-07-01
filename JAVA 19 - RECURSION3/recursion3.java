068f78195be8ea95db7f9a95472e0ab51bfc810b
Manthan3000 <manthanovhal695@gmail.com>
2024-07-01 21:20:48 +0530

    main

--- JAVA 19 - RECURSION3/recursion3.java
+++ JAVA 19 - RECURSION3/recursion3.java
@@ -1,4 +1,4 @@
-// // PRInt All Permutation of a STRING :
+// // PRInt All Permutation of a STRING : "ABC"
 
 // public class recursion3 {
 
@@ -53,10 +53,89 @@
 
 // Place Tiles of size 1 * m in a floor if size n * m
 
-public class recursion3 {
+// public class recursion3 {
 
-    public sta
+//     public static int placeTiles(int n, int m) {
+//         // BASE CASE
+//         if (n == m) {
+//             return 2;
+//         }
+//         if (n < m) {
+//             return 1;
+//         }
+
+//         // VERTICALLY
+//         int vertPlacement = placeTiles(n - m, m);
+
+//         // HORIZONTAL
+//         int horiPlacement = placeTiles(n - 1, m);
+
+//         return vertPlacement + horiPlacement;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 4, m = 2;
+
+//         System.out.println(placeTiles(n, m));
+
+//     }
+// }
+
+// Find the Number of Ways in which you can invite n people toyour party
+//single pr in pairs
+
+// public class recursion3 {
+//     public static int callGuest(int n) {
+//         if (n <= 1) {
+//             return 1;
+//         }
+
+//         // Single
+//         int ways1 = callGuest(n - 1);
+
+//         // Pairs
+//         int ways2 = (n - 1) * callGuest(n - 2);
+
+//         return ways1 + ways2;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 4;
+//         System.out.println(callGuest(n));
+//     }
+// }
+
+// Print All the Subset of a set of n Natural Numbers 
+// N = 3 = (1,2,3)
+
+import java.util.ArrayList;
+
+public class recursion3 {
+    public static void printsubset(ArrayList<Integer> subset) {
+        for (int i = -0; i < subset.size(); i++) {
+            System.out.println(subset.get(i) + "");
+        }
+        System.out.println();
+    }
+
+    public static void findSubset(int n, ArrayList<Integer> subset) {
+        if (n == 0) {
+            printsubset(subset);
+            return;
+        }
+        // add hoga
+
+        subset.add(n);
+        findSubset(n - 1, subset);
+
+        // add nahiihoga
+        subset.remove(subset.size() - 1);
+        findSubset(n - 1, subset);
+    }
+
     public static void main(String[] args) {
- 
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubset(n, subset);
     }
 }
