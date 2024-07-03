6a14c488aa68b0a7c61524355661a02c7c184122
Manthan3000 <manthanovhal695@gmail.com>
2024-07-03 19:29:34 +0530

    main

--- JAVA 20 - BACKTRACKING N QUEENS/backtracking.java
+++ JAVA 20 - BACKTRACKING N QUEENS/backtracking.java
@@ -1,5 +1,23 @@
+//Print Permuntations == n !
+
 public class backtracking {
-    public static void main(String[] args) {
-        System.out.println("java");
+
+    public static void print(String str, String perm, int idx) {
+        if (str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currchar = str.charAt(i);
+            String newstr = str.substring(0, i) + str.substring(i + 1);
+
+            print(newstr, perm + currchar, idx + 1);
+        }
     }
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        print(str, "", 0);
+    }
+
 }
