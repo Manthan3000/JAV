3c9041ce2eff18dca649796d4410f0c0c1700edb
Manthan3000 <manthanovhal695@gmail.com>
2024-07-05 23:08:37 +0530

    main

--- JAVA 24 OPPs ONE SHOT/3_abstraction/abstraction.java
+++ JAVA 24 OPPs ONE SHOT/3_abstraction/abstraction.java
@@ -8,29 +8,80 @@
 //2.Can not be instansianted( OBJECTS) BASE CLASS >>> DERIVED CLASS
 
 //BASE CLASS:
-abstract class animal {
-    public void walk() {
+// abstract class animal {
+//     public void walk() {
 
-    }
-}
+//     }
+// }
 
-// DERIVED CLASS :
-class Horse extends animal {
-    public void walk() {
-        System.out.println("Walk on 4 legs");
-    }
-}
+// // DERIVED CLASS :
+// class Horse extends animal {
+//     public void walk() {
+//         System.out.println("Walk on 4 legs");
+//     }
+// }
 
-class Chicken extends animal {
-    public void walk() {
-        System.out.println("Walk on 2 legs");
+// class Chicken extends animal {
+//     public void walk() {
+//         System.out.println("Walk on 2 legs");
+//     }
+// }
+
+// public class abstraction {
+//     public static void main(String[] args) {
+//         Horse horse = new Horse();
+//         horse.walk(); // Walk on 4 legs
+
+//     }
+// }
+
+//  PURE Abstraction :  INTERFACE
+
+// interface animal {
+//     int eyes = 2;
+
+//     void walk();
+// }
+
+// interface Herbivore {
+
+// }
+
+// // Multiple inheratance is done by " interface "
+
+// class Horse implements animal, Herbivore {
+//     public void walk() {
+//         System.out.println(" walk on 4 leg");
+//     }
+// }
+
+// public class abstraction {
+//     public static void main(String[] args) {
+//         Horse horse = new Horse();
+//         horse.walk();
+//     }
+// }
+
+//  Static Will be " Common " for all Objects  
+
+class Student {
+    String name;
+    static String school; // Static == School is same for all Student obj
+
+    // Update value of School Name :
+    public static void changeSchool() {
+        school = "New School";
     }
+
 }
 
 public class abstraction {
-    public static void main(String[] args) {
-        Horse horse = new Horse();
-        horse.walk(); // Walk on 4 legs
 
+    public static void main(String[] args) {
+
+        Student.school = "MMCOE";
+        Student s1 = new Student();
+        s1.name = "Manthan";
+        System.out.println(s1.school);
     }
 }
