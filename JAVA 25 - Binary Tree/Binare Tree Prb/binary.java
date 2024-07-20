1bd454d4800cf8e16ca3e9b54d5116c8ade0377b
Manthan3000 <manthanovhal695@gmail.com>
2024-07-20 22:39:25 +0530

    main

--- JAVA 25 - Binary Tree/Binare Tree Prb/binary.java
+++ JAVA 25 - Binary Tree/Binare Tree Prb/binary.java
@@ -155,10 +155,47 @@
 //     }
 // }
 
-// Interview Questionn....
-// 4 . Diameter Of a Tree : no. of nodes in longest path bett. any 2 Nodes
-// NODE is Included 
-//Time Complex O(n*n)
+// 5 . SubTree of Another Main Tree 
+
+//   LeetCode Question
+
+// import javax.swing.tree.TreeNode;
+
+// public class binary {
+//     public boolean isIdentical(TreeNode root, TreeNode subRoot) {
+//         if (root == null && subRoot == null) {
+//             return true;
+//         }
+//         if (root == null || subRoot == null) {
+//             return false;
+//         }
+//         if (root.val == subRoot.val) {
+//             return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+//         }
+
+//         return false;
+//     }
+
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if (subRoot == null) {
+//             return true;
+//         }
+//         if (root == null) {
+//             return false;
+//         }
+//         if (root.val == subRoot.val) {
+//             if (isIdentical(subRoot, subRoot)) {
+//                 return true;
+//             }
+//         }
+//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     }
+// }
+
+//   Interview Questionn....
+//   4 . Diameter Of a Tree : no. of nodes in longest path bett. any 2 Nodes
+//   NODE is Included 
+//   Time Complex O(n*n)
 
 // public class binary {
 
@@ -228,111 +265,102 @@
 // }
 
 // Approach 2 Node Is not Included
-//Time compx O(n) : Linear Time Complex
-
-// public class binary {
-
-//     static class Node {
-//         int data;
-//         Node left;
-//         Node right;
-
-//         Node(int data) {
-//             this.data = data;
-//             this.left = null;
-//             this.right = null;
-//         }
-//     }
-
-//     static class BinaryTree {
-
-//         static int idx = -1;
-
-//         public Node buildTree(int nodes[]) {
-//             idx++;
-//             if (nodes[idx] == -1) {
-//                 return null;
-//             }
-
-//             Node newNode = new Node(nodes[idx]);
-//             newNode.left = buildTree(nodes);
-//             newNode.right = buildTree(nodes);
-//             return newNode;
-//         }
-//     }
-
-//     public static int height(Node root) {
-//         if (root == null) {
-//             return 0;
-//         }
-//         int leftHeight = height(root.left);
-//         int rightHeight = height(root.right);
-
-//         int myHeight = Math.max(leftHeight, rightHeight) + 1;
-
-//         return myHeight;
-//     }
-
-//     static class TreeInfo {
-
-//         int ht;
-//         int diam;
-
-//         TreeInfo(int ht, int diam) {
-//             this.ht = ht;
-//             this.diam = diam;
-//         }
-//     }
-
-//     public static TreeInfo diameter2(Node root) {
-
-//         if (root == null) {
-//             return new TreeInfo(0, 0);
-//         }
-//         TreeInfo left = diameter2(root.left);
-//         TreeInfo right = diameter2(root.right);
-
-//         int myHeight = Math.max(left.ht, right.ht);
-
-//         int daim1 = left.diam;
-//         int daim2 = right.diam;
-//         int daim3 = left.ht + right.ht + 1;
-
-//         int mydaim = Math.max(Math.max(daim1, daim2), daim3);
-
-//         TreeInfo mInfo = new TreeInfo(myHeight, mydaim);
-//         return mInfo;
-//     }
-
-//     public static int Diameter(Node root) {
-//         if (root == null) {
-//             return 0;
-//         }
-
-//         int daim1 = Diameter(root.left);
-//         int daim2 = Diameter(root.right);
-//         int daim3 = height(root.left) + height(root.right) + 1;
-
-//         return Math.max(daim3, Math.max(daim1, daim2));
-
-//     }
-
-//     public static void main(String[] args) {
-//         int node[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-//         BinaryTree tree = new BinaryTree();
-//         Node root = tree.buildTree(node);
-//         System.out.println("Height of Tree = " + height(root));
-
-//         System.out.print(diameter2(root).diam);
-
-//     }
-// }
-
-// 5 . SubTree of Another Tree 
+// Time compx O(n) : Linear Time Complex
 
 public class binary {
 
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+
+        static int idx = -1;
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        int myHeight = Math.max(leftHeight, rightHeight) + 1;
+
+        return myHeight;
+    }
+
+    static class TreeInfo {
+
+        int ht;
+        int diam;
+
+        TreeInfo(int ht, int diam) {
+            this.ht = ht;
+            this.diam = diam;
+        }
+    }
+
+    public static TreeInfo diameter2(Node root) {
+
+        if (root == null) {
+            return new TreeInfo(0, 0);
+        }
+        TreeInfo left = diameter2(root.left);
+        TreeInfo right = diameter2(root.right);
+
+        int myHeight = Math.max(left.ht, right.ht);
+
+        int daim1 = left.diam;
+        int daim2 = right.diam;
+        int daim3 = left.ht + right.ht + 1;
+
+        int mydaim = Math.max(Math.max(daim1, daim2), daim3);
+
+        TreeInfo mInfo = new TreeInfo(myHeight, mydaim);
+        return mInfo;
+    }
+
+    public static int Diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int daim1 = Diameter(root.left);
+        int daim2 = Diameter(root.right);
+        int daim3 = height(root.left) + height(root.right) + 1;
+
+        return Math.max(daim3, Math.max(daim1, daim2));
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Subtree Of another Tree");
+        int node[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(node);
+        System.out.println("Height of Tree = " + height(root));
+
+        System.out.print(diameter2(root).diam);
+
     }
 }
